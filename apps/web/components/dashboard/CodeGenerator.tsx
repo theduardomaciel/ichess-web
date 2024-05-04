e3eb2258fd862093a5f9e4a057f4387337dfa816
Eduardo <eduardomacielbr@gmail.com>
2024-05-04 00:14:06 -0300

    feat: code generator backend and ui working

--- apps/web/components/dashboard/CodeGenerator.tsx
+++ apps/web/components/dashboard/CodeGenerator.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useEffect, useState } from "react";
 
 // Icons
 import ArrowRightIcon from "@/public/icons/arrow_right.svg";
 import ReloadIcon from "@/public/icons/reload.svg";
+import { Loader2 } from "lucide-react";
 
 // Components
 import { Button } from "@/components/ui/button";
@@ -16,65 +18,110 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog";
-import { useState } from "react";
-import { useTimer } from "@/hooks/use-timer";
+
+// API
+import { trpc } from "@/lib/trpc/react";
+import { Skeleton } from "../ui/skeleton";
+import { useToast } from "@/hooks/use-toast";
 
 interface CodeGeneratorProps {
 	charactersAmount?: number;
-	interval?: number;
+	lifetime?: number;
 }
 
-const DEFAULT_CHARACTERS_AMOUNT = 6;
+function resetProgress() {
+	const progressBar = document.getElementById("progress_bar");
 
-const generateCode = (charactersAmount?: number) => {
-	const code = Array.from(
-		{ length: charactersAmount || DEFAULT_CHARACTERS_AMOUNT },
-		() => Math.floor(Math.random() * 10),
-	);
-
-	localStorage.setItem("code", code.join(""));
-
-	return code;
-};
+	if (progressBar) {
+		progressBar.style.transition = "width 1s linear";
+		progressBar.style.width = "0%";
+	}
+}
 
 export function CodeGenerator({
 	charactersAmount,
-	interval = 30000,
+	lifetime = 30000,
 }: CodeGeneratorProps) {
-	const [code, setCode] = useState<number[] | undefined>(
-		localStorage.getItem("code")?.split("").map(Number),
-	);
+	const [code, setCode] = useState<string | undefined | null>(undefined);
+	const toast = useToast();
 
-	const { reset, time, stop, start } = useTimer({
-		timeInMs: interval,
-	});
+	const [currentDate, setCurrentDate] = useState<Date>(new Date());
+	const [expiringDate, setExpiringDate] = useState<Date | undefined>(undefined);
 
-	async function onNewCode() {
-		const progressBar = document.getElementById("progress_bar");
-		const newCode = generateCode(charactersAmount);
+	const remainingTime = expiringDate
+		? Math.max(expiringDate.getTime() - currentDate.getTime(), 0)
+		: 0;
 
-		if (progressBar) {
-			progressBar.style.transition = "none";
-			progressBar.style.width = "0%";
-		}
+	const mutation = trpc.getVerificationCode.useMutation();
 
-		setCode(newCode);
-		reset();
-		start();
+	async function generateCode() {
+		setCode(undefined);
 
-		console.log("New code generated:", newCode);
+		try {
+			let newCode: string | null = null;
+			let expires: Date | undefined = undefined;
 
-		if (progressBar) {
-			progressBar.style.transition = `width ${interval}ms linear`;
-			progressBar.style.width = "100%";
+			const localStorageExpires = localStorage.getItem("expires");
+
+			// Restore code if it's still valid
+			if (
+				localStorageExpires &&
+				new Date(localStorageExpires).getTime() > new Date().getTime()
+			) {
+				newCode = localStorage.getItem("code") || null;
+				expires = new Date(localStorageExpires);
+
+				localStorage.removeItem("code");
+				localStorage.removeItem("expires");
+
+				console.log("Restored code:", newCode, expires);
+			} else {
+				const { code: serverNewCode, expires: serverExpires } =
+					await mutation.mutateAsync({
+						charactersAmount,
+						lifetime,
+					});
+
+				newCode = serverNewCode;
+				expires = serverExpires;
+
+				console.log("Generated code:", newCode, expires);
+			}
+
+			if (newCode && expires) {
+				if (code) {
+					resetProgress();
+				}
+
+				setCode(newCode);
+				setExpiringDate(expires);
+
+				localStorage.setItem("code", newCode);
+				localStorage.setItem("expires", expires.toISOString());
+			} else {
+				setCode(null);
+			}
+		} catch (error) {
+			console.error("Error generating code:", error);
+			setCode(null);
 		}
 	}
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCurrentDate(new Date());
+		}, 1000);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
 
 	return (
 		<Dialog
 			onOpenChange={(isOpen) => {
-				if (isOpen) {
-					start();
+				if (isOpen && !code) {
+					generateCode();
 				}
 			}}
 		>
@@ -98,22 +145,49 @@ export function CodeGenerator({
 						})}
 					</DialogDescription>
 				</DialogHeader>
-				<ul className="flex w-full flex-row items-center justify-between gap-2.5">
-					{code?.map((character, i) => (
-						<li
-							key={i}
-							className="flex w-full items-center justify-center rounded-sm bg-gray-200 px-1 py-6 text-4xl font-extrabold text-neutral"
-						>
-							{character}
-						</li>
-					))}
-				</ul>
+				{code === undefined ? (
+					<ul className="flex w-full flex-row items-center justify-between gap-2.5">
+						{Array.from({ length: charactersAmount || 6 }, (_, i) => (
+							<li
+								key={i}
+								className="w-full h-[88px] bg-gray-200 rounded-sm animate-pulse"
+							/>
+						))}
+					</ul>
+				) : code === null ? (
+					<DialogDescription className="mx-auto my-12">
+						Ocorreu um erro ao gerar o código
+					</DialogDescription>
+				) : (
+					<ul className="flex w-full flex-row items-center justify-between gap-2.5">
+						{code?.split("").map((character, i) => (
+							<button
+								type="button"
+								key={i}
+								className="flex w-full items-center justify-center rounded-sm bg-gray-200 px-1 py-6 text-4xl font-extrabold text-neutral cursor-default group group-hover:bg-primary-100"
+								onClick={() => {
+									navigator.clipboard.writeText(code);
+									toast.toast({
+										title: "Código copiado",
+										description:
+											"O código foi copiado para a área de transferência",
+										variant: "success",
+									});
+								}}
+							>
+								{character}
+							</button>
+						))}
+					</ul>
+				)}
 				<div className="flex w-full h-3 rounded-full bg-gray-300">
 					<div
 						id="progress_bar"
-						className="flex h-full bg-primary-100 rounded-full"
+						className="h-full bg-primary-100 rounded-full transition-[width] ease-linear"
 						style={{
-							width: `${(time * 100) / interval}%`,
+							width: `${
+								code ? Math.max((remainingTime / lifetime) * 100, 0) : 0
+							}%`,
 						}}
 					/>
 				</div>
@@ -121,11 +195,15 @@ export function CodeGenerator({
 					className="group h-14 w-full justify-between p-4"
 					size={"xl"}
 					variant={"secondary"}
-					onClick={onNewCode}
-					disabled={time > 0}
+					onClick={generateCode}
+					disabled={remainingTime > 0 || !code}
 				>
 					Gerar novo código
-					<ReloadIcon className="group-hover:animate-one-spin" />
+					{code === undefined ? (
+						<Loader2 className="w-4 h-4 text-primary-100 animate-spin" />
+					) : (
+						<ReloadIcon className="group-hover:animate-one-spin" />
+					)}
 				</Button>
 				<DialogFooter className="w-full sm:justify-start">
 					<DialogClose asChild>
--- packages/api/routers/verificationCodes.ts
+++ packages/api/routers/verificationCodes.ts
@@ -3,6 +3,7 @@ import { db } from "@ichess/drizzle";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { verificationToken } from "@ichess/drizzle/schema";
+import { eq } from "@ichess/drizzle/orm";
 
 const DEFAULT_CHARACTERS_AMOUNT = 6;
 const DEFAULT_LIFETIME = 30000;
@@ -15,8 +16,36 @@ export const verificationCodesRouter = createTRPCRouter({
 				lifetime: z.number().optional(),
 			}),
 		)
-		.query(async ({ input }) => {
-			const { charactersAmount } = input;
+		.mutation(async ({ input, ctx }) => {
+			const { charactersAmount, lifetime } = input;
+			const userId = ctx.session.user.id;
+
+			if (!userId) {
+				throw new Error("User not found");
+			}
+
+			if (ctx.session.member?.role !== "admin") {
+				throw new Error("User is not an admin");
+			}
+
+			if (charactersAmount && charactersAmount < 1) {
+				throw new Error("Characters amount must be greater than 0");
+			}
+
+			const userTokens = await db
+				.select()
+				.from(verificationToken)
+				.where(eq(verificationToken.identifier, userId));
+
+			if (userTokens.length) {
+				for (const token of userTokens) {
+					if (token.expires.getTime() < Date.now()) {
+						await db
+							.delete(verificationToken)
+							.where(eq(verificationToken.token, token.token));
+					}
+				}
+			}
 
 			const code = Array.from(
 				{ length: charactersAmount || DEFAULT_CHARACTERS_AMOUNT },
@@ -24,16 +53,22 @@ export const verificationCodesRouter = createTRPCRouter({
 			);
 
 			const codeString = code.join("");
-			const expireDate = new Date(
-				Date.now() + (input.lifetime || DEFAULT_LIFETIME),
-			);
+			const expireDate = new Date(Date.now() + (lifetime || DEFAULT_LIFETIME));
 
-			await db.insert(verificationToken).values({
-				expiresAt: expireDate,
-				token: codeString,
-				identifier: "verification",
-			});
+			try {
+				await db.insert(verificationToken).values({
+					expires: expireDate,
+					token: codeString,
+					identifier: userId,
+				});
+			} catch (error) {
+				console.error("Error inserting verification token:", error);
+				throw new Error("Error generating verification code");
+			}
 
-			return codeString;
+			return {
+				code: codeString,
+				expires: expireDate,
+			};
 		}),
 });
